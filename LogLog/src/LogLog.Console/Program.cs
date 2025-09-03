e2cfee173131eb001a7af325fc619c98f8d766e3
artem <bikmaev.artem@gmail.com>
2025-09-03 21:50:58 +0400

    0004: added GroupEntity

--- LogLog/src/LogLog.Console/Program.cs
+++ LogLog/src/LogLog.Console/Program.cs
@@ -11,81 +11,15 @@ namespace LogLog.Console
 
         private static void Run()
         {
-            var isExit = false;
-            var command = string.Empty;
-
             do
             {
-                ClearConsole();
-                ShowCommands();
-                DoCommand(command);
+                // Показ контекста и инф.
+                // Чтение команды
+                // Валдация команды
+                // Исполнение команды
 
-                command = ReadCommand();
 
-                isExit = IsExitCommand(command);
-            } while (!isExit);
+            } while (false);
         }
-
-
-        private static void ShowCommands()
-        {
-            System.Console.WriteLine("Commands");
-            System.Console.WriteLine("exit, list, start <id>, stop <id>");
-            System.Console.WriteLine("create <name> <description>");
-            System.Console.WriteLine("createstart <name> <description>");
-            System.Console.WriteLine("");
-        }
-
-        private static void ClearConsole()
-        {
-            System.Console.Clear();
-        }
-
-        private static string ReadCommand()
-        {
-            System.Console.WriteLine();
-            System.Console.Write("Type command: ");
-
-            var command = System.Console.ReadLine() ?? string.Empty;
-            return command;
-        }
-
-        private static void DoCommand(string command)
-        {
-            var commandAndArgs = command.Split(' ');
-
-            switch (commandAndArgs[0].ToLower())
-            {
-                case "list":
-                    CommandList();
-                    break;
-                case "create":
-                    CommandCreate(commandAndArgs);
-                    break;
-
-
-                case "start":
-                    break;
-                case "stop":
-                    break;
-            }
-        }
-
-        private static bool IsExitCommand(string command)
-        {
-            return command.ToLower().Trim().IndexOf("exit") == 0;
-        }
-
-
-
-        private static void CommandCreate(string[] args)
-        {
-        }
-
-
-        private static void CommandList()
-        {
-        }
-
     }
 }
